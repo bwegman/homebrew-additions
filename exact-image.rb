741be885f3d72bdb89be6dfdbfe344d7ea3e2416
craig t smith <craig@42branches.co>
2013-11-27 02:02:07 +0000

    downgrade

--- exact-image.rb
+++ exact-image.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class ExactImage < Formula
   homepage 'http://www.exactcode.de/site/open_source/exactimage/'
-  url 'http://dl.exactcode.de/oss/exact-image/exact-image-0.8.9.tar.bz2'
-  sha1 'd68edb48df333d31b3f87723e487d1b3ee45fdbf'
+  url 'http://dl.exactcode.de/oss/exact-image/exact-image-0.8.8.tar.bz2'
+  sha1 '9f5dbc2a57a0eac8810bbfc58afc667aaa492503'
 
   depends_on :x11
   depends_on 'freetype'
