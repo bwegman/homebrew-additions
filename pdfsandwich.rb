05b4bbc1be1b3841cf55683f0646764d9a811715
craig t smith <craig@42branches.co>
2013-11-27 01:34:40 +0000

    builddeps

--- pdfsandwich.rb
+++ pdfsandwich.rb
@@ -5,22 +5,15 @@ class Pdfsandwich < Formula
   url 'http://sourceforge.net/projects/pdfsandwich/files/pdfsandwich%200.0.8/pdfsandwich-0.0.8.tar.bz2/download'
   sha1 '9cc15b712c88d72ed711ed82f48848476a9fb5ce'
 
+  depends_on 'gawk' => :build
+  depends_on 'ocaml' => :build
+
   depends_on 'imagemagick'
   depends_on 'ghostscript'
   depends_on 'exact-image'
   depends_on 'tesseract'
 
  def install
-    args = ["--prefix=#{prefix}",
-            "--without-python",
-            "--without-perl",
-            "--without-php",
-            "--without-libungif"]
-    args << "--without-libpng" unless build.with? 'libpng'
-    args << "--without-libjpeg" unless build.with? 'jpeg'
-    args << "--without-libtiff" unless build.with? 'libtiff'
-
-    # system "./configure", *args
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make", "install"
   end
