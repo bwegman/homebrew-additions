a0d8859ec8134e53216f06af5dc0ea8ed426ddd2
craig t smith <craig@42branches.co>
2013-11-27 01:23:48 +0000

    need pdfsandwhich

---
+++ pdfsandwich.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Pdfsandwich < Formula
+  homepage ''
+  url 'http://sourceforge.net/projects/pdfsandwich/files/pdfsandwich%200.0.8/pdfsandwich-0.0.8.tar.bz2/download'
+  sha1 '9cc15b712c88d72ed711ed82f48848476a9fb5ce'
+
+  depends_on 'imagemagick'
+  depends_on 'ghostscript'
+  depends_on 'exact-image'
+  depends_on 'tesseract'
+
+ def install
+    args = ["--prefix=#{prefix}",
+            "--without-python",
+            "--without-perl",
+            "--without-php",
+            "--without-libungif"]
+    args << "--without-libpng" unless build.with? 'libpng'
+    args << "--without-libjpeg" unless build.with? 'jpeg'
+    args << "--without-libtiff" unless build.with? 'libtiff'
+
+    system "./configure", *args
+    system "make", "install"
+  end
+end
